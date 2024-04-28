3bfa7f61d0f341d629b38e59cf52e9ef37ee5988
Ojas <karanjkarojas1905@gmail.com>
2024-04-28 20:04:12 +0530

    Updated for different no of measurements

--- angle_predictor.py
+++ angle_predictor.py
@@ -28,63 +28,70 @@ train_path = "Dataset/MNIST/train"
 train_imgs = os.listdir(train_path)
 
 
-M = 60
+# M = [10, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375]
+M = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 n_classes = 10
 N = 28 * 28
-centers = np.zeros((n_classes, M))
-counts = np.zeros(n_classes)
+accuracy = []
 
-phi = torch.load("models/phi_correct.pt")
+for l in range(len(M)):
+        
+    centers = np.zeros((n_classes, M[l]))
+    counts = np.zeros(n_classes)
 
-phi.requires_grad = False
-
-phi = phi.detach().numpy()
-
-
-
-for i in range(len(train_imgs)):
-    counts[map[train_imgs[i].split("_")[0]]] += 1
-    centers[map[train_imgs[i].split("_")[0]],:] += phi @ plt.imread(train_path + "/" + train_imgs[i]).flatten()
+    phi = np.random.randn(M[l], N)
 
 
-for i in range(10):
-    centers[i,:] = centers[i,:]/counts[i]
+    for i in range(len(train_imgs)):
+        counts[map[train_imgs[i].split("_")[0]]] += 1
+        centers[map[train_imgs[i].split("_")[0]],:] += phi @ plt.imread(train_path + "/" + train_imgs[i]).flatten()
 
 
-test_path = "Dataset/MNIST/test"
-test_imgs = os.listdir(test_path)
-
-x = []
-y = []
-
-for i in range(len(test_imgs)):
-    x.append(rotate_img(plt.imread(test_path + "/" + test_imgs[i]), np.random.randint(0, 360)))
-    y.append(map[test_imgs[i].split("_")[0]])
-
-thetas = np.arange(5, 360, 10)
-
-preds = []
-
-for i in range(len(x)):
-    pred = 0
-    min_dist = np.inf
-    min_angle = np.inf
-
-    for j in range(len(thetas)):
-        r_img = rotate_img(x[i], -thetas[j])
-        meas = phi @ r_img.flatten()
-
-        for k in range(len(centers)):
-            if(np.linalg.norm(meas - centers[k,:]) < min_dist):
-                min_dist = np.linalg.norm(meas - centers[k,:])
-                min_angle = thetas[j]
-                pred = k
-    preds.append(pred)
+    for i in range(10):
+        centers[i,:] = centers[i,:]/counts[i]
 
 
-c = 0
-for i in range(len(preds)):
-    if(preds[i] == y[i]):
-        c += 1
+    test_path = "Dataset/MNIST/test"
+    test_imgs = os.listdir(test_path)
 
-print("classification accuracy ->", 100*c/len(preds))
+    x = []
+    y = []
+
+    for i in range(len(test_imgs)):
+        x.append(rotate_img(plt.imread(test_path + "/" + test_imgs[i]), np.random.randint(0, 360)))
+        y.append(map[test_imgs[i].split("_")[0]])
+
+    thetas = np.arange(5, 360, 10)
+
+    preds = []
+
+    for i in range(len(x)):
+        pred = 0
+        min_dist = np.inf
+        min_angle = np.inf
+
+        for j in range(len(thetas)):
+            r_img = rotate_img(x[i], -thetas[j])
+            meas = phi @ r_img.flatten()
+
+            for k in range(len(centers)):
+                if(np.linalg.norm(meas - centers[k,:]) < min_dist):
+                    min_dist = np.linalg.norm(meas - centers[k,:])
+                    min_angle = thetas[j]
+                    pred = k
+        preds.append(pred)
+
+
+    c = 0
+    for i in range(len(preds)):
+        if(preds[i] == y[i]):
+            c += 1
+
+    # print("classification accuracy ->", 100*c/len(preds))
+    accuracy.append(100*c/len(preds))
+    print(l, "th iteration done")
+
+plt.plot(M, accuracy)
+plt.xlabel("No. of meansurements")
+plt.ylabel("Classification accuracy")
+plt.show()
